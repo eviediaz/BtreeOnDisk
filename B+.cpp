b8fc67850086c11eb7fd49e4ad1a0577e7efcc45
Skeletus <83602040+Skeletus@users.noreply.github.com>
2024-06-09 19:45:15 -0500

    refactor bplus tree cpp

--- B+.cpp
+++ B+.cpp
@@ -1,16 +1,21 @@
 #include <vector>
 #include <algorithm> // para std::lower_bound y std::upper_bound
 
-struct Peruvian{
-    uint64_t dni;
+struct Peruvian {
+    char dni[8];
     uint64_t idPosition;
+
+    Peruvian(const char* dni_input, uint64_t id) : idPosition(id) {
+        std::strncpy(dni, dni_input, 8);
+    }
 };
 
+template <typename T, typename GenericData>
 class BPlusTreeNode {
 public:
     bool isLeaf; // indica si este nodo es una hoja o un nodo interno
-    std::vector<uint64_t> keys; // DNIs (ordenados)
-    std::vector<Peruvian*> data; // punteros a objetos de ciudadanos (ordenados por keys)
+    std::vector<T> keys; // DNIs (ordenados)
+    std::vector<GenericData*> data; // punteros a objetos de ciudadanos (ordenados por keys)
     BPlusTreeNode* parent; // puntero al nodo padre
     std::vector<BPlusTreeNode*> children; // punteros a nodos hijos
 
@@ -20,18 +25,18 @@ public:
     /// @brief Insertar un nuevo registro de ciudadano en este nodo
     /// @param dni
     /// @param citizen 
-    void insert(uint64_t dni, Peruvian* citizen) {
+    void insert(const T& key, GenericData* value) {
         // Búsqueda binaria para encontrar la posición correcta para la inserción
-        auto it = std::lower_bound(keys.begin(), keys.end(), dni);
-
-        // Insertar el DNI en la posición correcta
-        keys.insert(it, dni);
+        auto it = std::lower_bound(keys.begin(), keys.end(), key);
 
         // Calcular la posición para insertar el ciudadano
         size_t index = it - keys.begin();
 
+        // Insertar el DNI en la posición correcta
+        keys.insert(it, key);
+
         // Insertar el ciudadano en la posición correcta
-        data.insert(data.begin() + index, citizen);
+        data.insert(data.begin() + index, value);
 
         // Si el nodo se sobrepasa, hay que dividirlo
         if (keys.size() > maxKeys) {
@@ -50,13 +55,13 @@ public:
     /// @brief Buscar un peruano por DNI dentro de este nodo
     /// @param dni 
     /// @return el puntero correspondiente a Citizen* si se encuentra, de lo contrario nullptr
-    Peruvian* search(uint64_t dni) {
+    GenericData* search(const T& key) {
         // Búsqueda binaria para encontrar la posición correcta
-        auto it = std::lower_bound(keys.begin(), keys.end(), dni);
+        auto it = std::lower_bound(keys.begin(), keys.end(), key);
         size_t index = it - keys.begin();
 
         // Comprobar si el DNI se encuentra en las keys
-        if (it != keys.end() && *it == dni) {
+        if (it != keys.end() && *it == key) {
             return data[index];
         }
 
@@ -65,7 +70,7 @@ public:
             return nullptr;
         } else {
             // Buscar en el nodo hijo correspondiente
-            return children[index]->search(dni);
+            return children[index]->search(key);
         }
     }
 
